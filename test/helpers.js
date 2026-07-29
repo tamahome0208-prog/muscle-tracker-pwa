582cfaea0ac73c98759ea2c95da9f30d1d3f7c9d
tamah <tamahome0208@gmail.com>
2026-07-30 00:21:15 +0900

    chore: プロジェクト初期化とテスト環境

---
+++ test/helpers.js
@@ -0,0 +1,10 @@
+export function memoryStorage(initial = {}) {
+  const map = new Map(Object.entries(initial));
+  return {
+    getItem: (k) => (map.has(k) ? map.get(k) : null),
+    setItem: (k, v) => map.set(k, String(v)),
+    removeItem: (k) => map.delete(k),
+    key: (i) => [...map.keys()][i] ?? null,
+    get length() { return map.size; }
+  };
+}
